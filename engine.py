a144bd27dacc4548f668ff1bc63af6aeec8d9c2b
Christian Ivan Ortega Gonzalez <christiani.ortega@softtekclients.com>
2020-04-05 23:58:15 -0700

    version 1  baristas are threads

--- engine.py
+++ engine.py
@@ -1,29 +1,28 @@
 
+import time
 class Barista:
     def __init__(self):
         self._id = None
         self.state = None
         self.name = None
+        
 
-    def makeDrink(self,drinkId):
-        # Make Drink 
-        return results
+    def makeDrink(self,drink = None):        
+        # Make Drink
+        print ("worker " + str(self._id) + " is starting making  drink " + str(drink.order_id))
+        drink.brew()        
+        print ("worker " + str(self._id) + " is done making  drink " + str(drink.order_id))
+
+
 
 class Drink:
     def __init__(self):
-        self._id = None
-        self.name = None
-        self.execTime = None
-        self.price = None
-        self.priority = None
+        self.order_id = None
+        self.order_time = None
+        self.brew_time = None        
+        self.type = None
+        self.state = None                
 
-    def makeMe(self):
-        import time
-        try:
-            self.execTime
-        except Exception as e:
-            self.execTime = 60
-        # we can put it to sleep, but i like to simulate some cpu time to check performance
-        for i in range(1, self.execTime):
-            self.execTime / self.execTime * 2
-            time.sleep(1)  
+    def brew(self):        
+        time.sleep(self.order_time)
+        
--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from engine import Barista,Drink
+import os
 
+import json
+import sys
+
+import threading
+import time
+
+import random 
 class Shop:
     def __init__(self):
         self.shift = 100
+        self.start_shift = 0
         # a db would be better 
         self.drinks =  [
             { "type": "tea",      "brew_time": 3, "profit": 2 },
@@ -10,11 +19,85 @@ class Shop:
             { "type": "affogato", "brew_time": 7, "profit": 5 }
         ]
         self.baristas = [
-            {"id":1,"name":"John"},
-            {"id":2,"name":"Peter"}
-        ]
+            {"id":1},
+            {"id":2}
+        ]        
+        self.config = None
+
+        self.threads = []
+        self.tindex = 0
+        self.pool = []     
+
+        self.pending_orders_queue = []
+        self.done_orders_queue = []
+       
+
+    def openConfig(self):                
+        try:
+            f = open("config.json", "r")
+            self.config = json.loads(f.read())        
+        except Exception as e:
+            print ("error 1: config error =  " + str(e))
+            sys.exit(0)
+
+    def getOrders(self):
+        try:            
+            return self.config['orders']
+        except Exception as e:
+            print ("error 2: config error =  " + str(e))
+            sys.exit(0)            
+
+    def worker(self,barista):
+        # workers will work until list of orders is empty after that they die
+        print (str(barista._id) + " is working" )
+        # get the a drink
+        
+        while(True):    
+            if len(self.pending_orders_queue) <= 0:
+                break
+
+            for order in self.pending_orders_queue:
+                if order.state == "done":
+                    continue
+                else:
+                    tmpore = order
+                    self.pending_orders_queue.remove(order)
+                    barista.makeDrink(tmpore)                
+                    self.done_orders_queue.append(tmpore)                    
+                    break
+
+
+    def OpenStore(self):
+        # get orders
+        # create the queue of orders so workers can get roders work on them until they are empty, if a new order comes triug it must be added to the queue
+        self.pending_orders_queue = []
+        self.done_orders_queue = []
+        orders = self.getOrders()
+        # order by order time
+        orders = sorted(orders, key = lambda i: i['order_time']) 
+        
+        for order in orders:
+            tmpdrink = Drink()
+            tmpdrink.order_id = order['order_id']
+            tmpdrink.order_time = order['order_time']
+            tmpdrink.type = order['type']
+            tmpdrink.state = "pending" # using simple text just for demo
+            self.pending_orders_queue.append(tmpdrink)
+            
+        # create the workers they will select the drinks based on the order time (asc) and state, once is brew they will change the state to done, preferly to putting on another queeue of donde orders
+        for b in self.baristas:
+            barista = Barista()
+            barista._id = b['id']
+            t = threading.Thread(target=self.worker,args=(barista,))
+            self.threads.append({'index':b['id'],'thread':t})
+            t.start()              
+            
+        
+
 
 if __name__ == "__main__":
     shop = Shop()
+    shop.openConfig()
+    shop.OpenStore()
         
 
